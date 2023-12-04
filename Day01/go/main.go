be03fab6900bed484befdc0320cfbeda2f0d9163
zvdy <cristian.030902@gmail.com>
2023-12-04 21:57:57 +0100

    Day01

--- Day01/go/main.go
+++ Day01/go/main.go
@@ -6,9 +6,32 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func main() {
+var matchmap = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"0":     0,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+}
+
+func part1() {
 	// Open the input file
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -42,5 +65,67 @@ func main() {
 	}
 
 	// Print the total sum
-	fmt.Println(sum)
+	fmt.Println("Part one:", sum)
+}
+
+func part2() {
+	// Open the input file
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Create a new Scanner for the file
+	scanner := bufio.NewScanner(file)
+	var input []string
+
+	// Loop over all lines in the file and add them to the input slice
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	// Check for scanning error
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	sum := 0
+
+	// Loop over all lines in the input slice
+	for _, ln := range input {
+		first := -1
+		last := -1
+		minpos := len(ln) + 1
+		maxpos := -1
+
+		// Loop over all keys in the matchmap
+		for k, v := range matchmap {
+			p := strings.Index(ln, k)
+			if p == -1 {
+				// substring not found
+				continue
+			}
+			// substring found
+			if p < minpos {
+				first = v
+				minpos = p
+			}
+
+			// Check if the substring is the last one
+			p = strings.LastIndex(ln, k)
+			if p > maxpos {
+				last = v
+				maxpos = p
+			}
+		}
+		sum += 10*first + last
+	}
+	fmt.Printf("Part two: %v\n", sum)
+}
+
+func main() {
+	part1()
+	part2()
 }
--- Day01/py/main.py
+++ Day01/py/main.py
@@ -1,22 +1,86 @@
 import re
 
-# Open the input file
-with open('input.txt', 'r') as file:
-    lines = file.readlines()
+# Dictionary to match string representation of numbers to their integer values
+matchmap = {
+    "one":   1,
+    "two":   2,
+    "three": 3,
+    "four":  4,
+    "five":  5,
+    "six":   6,
+    "seven": 7,
+    "eight": 8,
+    "nine":  9,
+    "0":     0,
+    "1":     1,
+    "2":     2,
+    "3":     3,
+    "4":     4,
+    "5":     5,
+    "6":     6,
+    "7":     7,
+    "8":     8,
+    "9":     9,
+}
 
-sum = 0
-regex = re.compile('\d')
+def part1():
+    # Open the input file
+    with open("input.txt", "r") as file:
+        lines = file.readlines()
 
-# Loop over all lines in the file
-for line in lines:
-    digits = regex.findall(line)
-    # Check if there are any digits
-    if len(digits) > 0:
-        # Get first and last digit and sum them
-        first_digit = digits[0]
-        last_digit = digits[-1]
-        two_digit_number = int(first_digit + last_digit)
-        sum += two_digit_number
+    sum = 0
+    regex = re.compile(r'\d')  # Regular expression to match digits
 
-# Print the total sum
-print(sum)
+    # Loop over all lines in the file
+    for line in lines:
+        digits = regex.findall(line)  # Find all digits in the line
+        # Check if there are any digits
+        if len(digits) > 0:
+            # Get first and last digit and sum them
+            first_digit = digits[0]
+            last_digit = digits[-1]
+            two_digit_number = int(first_digit + last_digit)  # Convert string to integer
+            sum += two_digit_number
+
+    # Print the total sum
+    print("Part one:", sum)
+
+def part2():
+    # Open the input file
+    with open("input.txt", "r") as file:
+        lines = file.readlines()
+
+    sum = 0
+
+    # Loop over all lines in the input
+    for line in lines:
+        first = -1
+        last = -1
+        minpos = len(line) + 1
+        maxpos = -1
+
+        # Loop over all entries in the matchmap
+        for k, v in matchmap.items():
+            p = line.find(k)  # Find the position of the key in the line
+            if p == -1:
+                # substring not found
+                continue
+            if p < minpos:
+                first = v
+                minpos = p
+
+            p = line.rfind(k)  # Find the last position of the key in the line
+            if p > maxpos:
+                last = v
+                maxpos = p
+
+        sum += 10*first + last
+
+    print("Part two:", sum)
+
+def main():
+    part1()
+    part2()
+
+if __name__ == "__main__":
+    main()
